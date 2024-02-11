d034694f06b1a1bd574806cc5ccd0accf49f6fb1
unknown <leandroezequielpedercini@gmail.com>
2024-02-11 02:07:47 -0300

    Bienvenido a Cabaña Puerto Mopane

---
+++ javascript/las-cabañas.js
@@ -0,0 +1,30 @@
+/* las-cabañas.html */
+
+// header //
+const cabezal = document.getElementById("cabezal-index-HTML")
+cabezal.innerHTML = '<img src="header/Puerto Mopane.png" alt="Puerto Mopane Cabañas" class="puerto-mopane-cabañas"> <img src="header/Facebook.png" alt="Facebook" class="facebook" > <img src="header/instagram.png" alt="Instagram" class="instagram" > <img src="header/whatsapp.png" alt="Whatsapp" class="whatsapp" > <img src="header/Movil.png" alt="Movil" class="movil" > <img src="header/mail.png" alt="E-mial" class="e-mail" > '  
+
+// Links //
+const links = document.getElementById("links")
+links.innerHTML = '<a" class="nuestroLugar-HTML">Nuestro lugar</a> <a href="las-cabañas.html" class="lasCabañas-HTML">Las cabañas</a> <a href="ubicacion.html" class="ubicacion-HTML">Ubicacion</a><a href="contacto.html" class="contacto-HTML">Contacto</a>'
+
+
+/* Imagen */
+const imagen = document.getElementById("imagen")
+imagen.innerHTML = '<img src="carrusel/3.jpeg" alt="Paisaje" class="imagen-portada">' 
+
+/* Descripcion */
+const descripcion1 = document.getElementById("descripcion-1")
+descripcion1.innerHTML = '<p class="descripcion1-dep1" >El complejo incluye una amplia piscina con deck y solarium, canchas de voley, fútbol y juegos para niños.</p> <p class="descripcion1-dep2">Amplias y cómodas cabañas de 2 a 6 personas. Las mismas cuentan con detalles de confort y categoría contando con todos los servicios  necesarios para que su estadia sea inolvidable.</p> '
+
+/* Descripcion2 */
+const Descripcion2 = document.getElementById("descripcion-2")
+Descripcion2.innerHTML = ' <h2 class="Equipamiento">Equipamiento:</h2> <p ><h6 class="descripcion2-dep1">baño privado (completo), Todas las cabañas cuentan con cocina totalmente equipada, microondas, heladeras, ventiladores, aire acondicionado (frio-calor), mantas y cobertores. TV por cable, internet Wifi y parrillas. Deck privado y cocheras individuales con media sombra dentro del predio</h6> .</p> <p class="descripcion2-dep2">DISCULPEN LAS MOLESTIAS, NO ACEPTAMOS MASCOTAS</p> <p class="descripcion2-dep3">Selecciona la imagen de la cabaña de tu gusto y descubri sus caracteristicas</p>'
+
+// Imagen-atardecer //
+const imagenatardecer = document.getElementById("imagen-atardecer")
+imagenatardecer.innerHTML = '<img src="footter/atardecer.jpeg" alt="Atardecer" class="imagen-atardecer">'
+
+
+const tarjeta = document.getElementById("tarjeta")
+tarjeta.innerHTML = '  <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> 2 <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> 3 <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> 4 <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> 5 <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> 6 <div class="card" style="width: 18rem;"> <img src="..." class="card-img-top" alt="..."> <div class="card-body"> <h5 class="card-title">Card title</h5> <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p> <a href="#" class="btn btn-primary">Go somewhere</a> </div> </div> '
